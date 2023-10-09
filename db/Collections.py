9a96b773376eef2f350e5b7d82d2132c68ced138
Eytes <yura.shamanov2000@bk.ru>
2023-10-09 11:30:08 +0200

    Создали коллекции в БД

--- db/Collections.py
+++ db/Collections.py
@@ -1,4 +1,11 @@
 import pymongo
+from pymongo.collection import Collection
 
-client = pymongo.MongoClient('localhost', 27017)
-db = client['EVENTO']
+__client = pymongo.MongoClient('localhost', 27017)
+__db = __client['EVENTO']
+
+
+agents: Collection = __db['agents']
+events: Collection = __db['events']
+appointments: Collection = __db['appointments']
+clients: Collection = __db['clients']
