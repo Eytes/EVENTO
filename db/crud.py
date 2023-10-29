a29b036073a21b5f0ef7bc1a45501c127847b007
Eytes <yura.shamanov2000@bk.ru>
2023-10-29 13:20:18 +0200

    появилась возможность создавать записи на прием (AppointmentCRUD)

--- db/crud.py
+++ db/crud.py
@@ -1,12 +1,13 @@
 from abc import ABC
 
 from pymongo.collection import Collection
+from pydantic import UUID4
 
 from Collections import (
     agents,
     events,
     appointments,
-    clients
+    clients,
 )
 
 from models import (
@@ -17,19 +18,19 @@ from models import (
 )
 
 
-class __MongoCRUD(ABC):
+class _MongoCRUD(ABC):
     """
     абстрактный класс для работы с mongodb
     """
 
     @staticmethod
-    def _create_document(collection: Collection, document: dict) -> str:
+    def create_document(collection: Collection, document: dict) -> str:
         """ Function to insert a document into a collection and
         return the document's id. """
         return str(collection.insert_one(document).inserted_id)
 
     @staticmethod
-    def _get_document(collection: Collection, elements: dict, multiple: bool = False) -> list:
+    def get_document(collection: Collection, elements: dict, multiple: bool = False) -> list:
         """ Function to retrieve single or multiple documents from a provided
         Collection using a dictionary containing a document's elements. """
         if multiple:
@@ -39,23 +40,24 @@ class __MongoCRUD(ABC):
         return [r for r in results]
 
     @staticmethod
-    def _update_document(collection: Collection, query_elements: dict, new_values: dict) -> None:
+    def update_document(collection: Collection, query_elements: dict, new_values: dict) -> None:
         """ Function to update a single document in a collection. """
         collection.update_one(query_elements, {'$set': new_values})
 
     @staticmethod
-    def _delete_document(collection: Collection, query: dict) -> None:
+    def delete_document(collection: Collection, query: dict) -> None:
         """ Function to delete a single document from a collection. """
         collection.delete_one(query)
 
 
-class AgentCRUD(__MongoCRUD):
+class AgentCRUD:
     __collection: Collection = agents
 
     @staticmethod
     def create(agent: Agent) -> str:
         """ Создание агента. Агент - представитьель и организатор события """
-        return super()._create_document(
+
+        return _MongoCRUD.create_document(
             AgentCRUD.__collection,
             agent.model_dump()
         )
@@ -63,77 +65,95 @@ class AgentCRUD(__MongoCRUD):
     @staticmethod
     def get_by_title(title: str) -> list:
         """ Получеение данных о агенте """
-        return super()._get_document(
+
+        return _MongoCRUD.get_document(
             AgentCRUD.__collection,
             {"title": title}
         )
 
 
-class EventCRUD(__MongoCRUD):
+class EventCRUD:
     __collection: Collection = events
 
     @staticmethod
     def create(event: Event) -> str:
         """ Создание события """
-        return super()._create_document(
+
+        return _MongoCRUD.create_document(
             EventCRUD.__collection,
             event.model_dump()
         )
 
     @staticmethod
-    def get_by_title(title: str) -> list:
+    def get_by_title(title: str) -> list[Event]:
         """ Получеение данных о событии """
-        return super()._get_document(
+
+        data = _MongoCRUD.get_document(
             EventCRUD.__collection,
             {"title": title},
             multiple=True
         )
+        return [Event(**item) for item in data]
 
 
-class AppointmentCRUD(__MongoCRUD):
+class AppointmentCRUD:
     __collection: Collection = appointments
 
     @staticmethod
-    def make_an_appointment(appointment: Appointment, client_id: int) -> None:
+    def make_an_appointment(appointment: Appointment, client_id: UUID4) -> None:
         """ Запись клиента на событие  """
-        super()._update_document(
+        _MongoCRUD.update_document(
             AppointmentCRUD.__collection,
-            {"_id": appointment.id_},
+            {"_id": appointment.id},
             {"client_id": client_id}
         )
 
     @staticmethod
     def create(appointment: Appointment) -> str:
         """ Создание записи на событие """
-        return super()._create_document(
+        return _MongoCRUD.create_document(
             AppointmentCRUD.__collection,
-            appointment.model_dump()
+            appointment.model_dump(by_alias=True)
         )
 
     @staticmethod
-    def get_by_client_id(client_id: int) -> list:
+    def get_by_client_id(client_id: UUID4) -> list:
         """ Получеение данных о записи клиента """
-        return super()._get_document(
+        return _MongoCRUD.get_document(
             AppointmentCRUD.__collection,
             {'client_id': client_id}
         )
 
+    @staticmethod
+    def get_by_event_id(event_id: UUID4) -> list:
+        """ Получеение данных о записи клиента """
 
-class ClientCRUD(__MongoCRUD):
+        data = _MongoCRUD.get_document(
+            AppointmentCRUD.__collection,
+            {'event_id': event_id},
+            multiple=True
+        )
+
+        return [Appointment(**item) for item in data]
+
+
+class ClientCRUD:
     __collection: Collection = clients
 
     @staticmethod
     def create(client: Client) -> str:
         """ Создание клиента """
-        return super()._create_document(
+        return _MongoCRUD.create_document(
             ClientCRUD.__collection,
             client.model_dump()
         )
 
     @staticmethod
-    def get_by_id(id_: int) -> list:
+    def get(client_id: UUID4) -> Client:
         """ Получеение данных о клиенте """
-        return super()._get_document(
+        client_data = _MongoCRUD.get_document(
             ClientCRUD.__collection,
-            {'_id': id_}
-        )
+            {'_id': client_id}
+        )[0]
+        client = Client(**client_data)
+        return client
