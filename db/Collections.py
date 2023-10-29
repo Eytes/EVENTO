e101c143230dd86fb97bb30d2a5e88cad284ed04
Eytes <yura.shamanov2000@bk.ru>
2023-10-29 20:57:37 +0200

    добавил TODO

--- db/Collections.py
+++ db/Collections.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 from pymongo.collection import Collection
 
-# uuidRepresentation='standard' (для поддержания кроссплатформенности)
+# uuidRepresentation='standard' для поддержания кроссплатформенности так как
+# в разных инструментах могут браться по разному биты при сериализации
 __client = MongoClient('localhost', 27017, uuidRepresentation='standard')
 __db = __client['EVENTO']
 
@@ -9,3 +10,5 @@ agents: Collection = __db['agents']
 events: Collection = __db['events']
 appointments: Collection = __db['appointments']
 clients: Collection = __db['clients']
+
+# TODO: добавить индексы для event_id, client_id
